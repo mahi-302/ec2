5e532fdddc96dbc057871e2c5cb349305e177d9c
BODA MAHESH KUMAR <93734751+mahi-302@users.noreply.github.com>
2022-01-04 22:56:09 +0530

    Update source.tf

--- source.tf
+++ source.tf
@@ -2,5 +2,5 @@ provider "aws" {
   region = "us-east-2"
 }
 module "ec2_instance"{
-    source = "git::https://github.com/Ramana4u/test.git"
+    source = "git::https://github.com/mahi-302/terracodes.git"
 }
