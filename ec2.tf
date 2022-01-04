86789cd4db0a53d2168f092bd009f3ef8f8e7a2c
BODA MAHESH KUMAR <93734751+mahi-302@users.noreply.github.com>
2022-01-04 22:53:37 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "abcd" {
      ami = "ami-0fb653ca2d3203ac1"
      instance_type = "t2.micro"
+     user-data = "${file("userdata.sh")}"
      availability_zone = "us-east-2c"
      subnet_id = "subnet-02ac0c9e52da309ab"
      tags = {
